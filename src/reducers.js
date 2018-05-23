5744ecfab74f906c8fd5571aff2980edb7a9f89e
chris.bridges <christopher_bridges@icloud.com>
2018-05-23 15:20:02 -0400

    reducers in place. implementing now

---
+++ src/reducers.js
@@ -0,0 +1,47 @@
+import {RESTART_GAME, MAKE_GUESS} from './actions';
+
+const initialState = {
+  guesses: [],
+  feedback: 'Make your guess!',
+  auralStatus: '',
+  correctAnswer: Math.round(Math.random() * 100) + 1
+};
+
+export const gameReducer = (state=initialState, action) => {
+  if (action.type === RESTART_GAME) {
+    return Object.assign({}, state, {
+      guesses: [],
+      feedback: 'Make your guess!',
+      auralStatus: '',
+      correctAnswer: Math.round(Math.random() * 100) + 1
+    });
+  }
+
+  if (action.type === MAKE_GUESS) {
+    guess = action.guess;
+    guess = parseInt(guess, 10);
+    if (isNaN(guess)) {
+      return Object.assign({}, state, {feedback: 'Please enter a valid number' });
+    }
+
+    const difference = Math.abs(guess - state.correctAnswer);
+
+    let feedback;
+    if (difference >= 50) {
+      feedback = 'You\'re Ice Cold...';
+    } else if (difference >= 30) {
+      feedback = 'You\'re Cold...';
+    } else if (difference >= 10) {
+      feedback = 'You\'re Warm.';
+    } else if (difference >= 1) {
+      feedback = 'You\'re Hot!';
+    } else {
+      feedback = 'You got it!';
+    }
+
+    return Object.assign({}, state, {
+      feedback,
+      guesses: [...state.guesses, guess]
+    });
+  }
+};
